e44c04d733be9ccb380f4c4b393ee962788e6413
Keka Nandi <kn262@afsconnect1.njit.edu>
2017-12-10 20:45:25 -0500

    Added to handle null To-Do list

---
+++ pages/errors.php
@@ -0,0 +1,37 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <!-- Bootstrap core CSS
+    <link href="../../../../dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+          integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+
+</head>
+<body>
+
+
+<div class="container">
+
+    <form class="form-signin">
+        <h2><br>
+
+            <?php
+            if(!empty($_GET['msg'])) {
+                $errorMsg = $_GET['msg'];
+                echo $errorMsg ;
+            }
+            ?>
+        </h2>
+    </form>
+</div>
+</body>
+</html>
+
