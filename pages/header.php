103b1cc5030c8dc7f62866f2e2bc2b21fb5a6c4e
Keka Nandi <kn262@afsconnect1.njit.edu>
2017-12-11 19:39:52 -0500

    Include header

---
+++ pages/header.php
@@ -0,0 +1,35 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <!-- Bootstrap core CSS
+    <link href="../../../../dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+          integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+
+</head>
+<body>
+
+<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+    <div class="container-fluid" >
+        <div class="navbar-header">
+            <span class="navbar-brand mb-0 h1">TO-DO LIST</span>
+        </div>
+
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-arrow-left"></span> Home</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-arrow-left"></span> Back</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Log Out</a></li>
+        </ul>
+    </div>
+</nav>
+
+</body>
+</html>
