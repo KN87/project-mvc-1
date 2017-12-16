8fbda71242ce3f84e36f79d7485a8b72381db28e
Keka Nandi <kn262@afsconnect1.njit.edu>
2017-12-16 02:47:15 -0500

    Fixed email field

--- pages/new_task.php
+++ pages/new_task.php
@@ -4,13 +4,15 @@
 <head>
     <meta charset="utf-8">
 
-    <meta charset="utf-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
-          integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 <?php
@@ -29,7 +31,7 @@ $ownerId = $_SESSION["userID"];
         <div class="form-group row">
             <label for="owneremail" class="col-sm-2 col-form-label">Owner Email:</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" id="owneremail" name="owneremail" placeholder="Enter Email xyz@abc.com">
+                <input type="email" class="form-control" id="owneremail" name="owneremail" placeholder="Enter Email xyz@abc.com">
             </div>
 
             <label for="ownerid" class="col-sm-2 col-form-label">Owner Id:</label>
